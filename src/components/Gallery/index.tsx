507f2ec3143a471a11bc8b259523d966447de59d
Stéphane <stephane1920@gmail.com>
2022-08-08 15:52:43 +0200

    add ligthouse for gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,50 +1,45 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, MouseEvent } from 'react';
 import { PortfolioData } from '~/interfaces/Data.intf';
 import { firstLetterUpper } from '~/utils/formatString';
 import './style.scss';
 
 type GalleryProps = {
- portfolioData?: PortfolioData[]
+ portfolioData?: PortfolioData[],
+ onClick?: (imageData: PortfolioData) => void
 }
 
-const Gallery: FunctionComponent<GalleryProps> = ({portfolioData = []}) => {
-  const [imgPopup, setImgPopup] = useState<PortfolioData>()
+const Gallery: FunctionComponent<GalleryProps> = ({portfolioData = [], onClick}) => {
+  const handleClick = (e: MouseEvent<HTMLDivElement>, imageData: PortfolioData) => {
+    e.preventDefault()
+    if(onClick) onClick(imageData)
+  }
 
   return (
-    <div className='gallery'>
-      { imgPopup ? (
-        <div className='gallery__popup'>
-          <div className='gallery__popup__content'>
-            <div className='gallery__popup__content__close'></div>
-            <img src={imgPopup?.imgFile} alt={imgPopup?.imgAlt} />
+    <div className='gallery gallery__grid'>   
+      { portfolioData.map((imageData, index) => (
+        <div key={imageData.id} 
+          onClick={(e) => handleClick(e, imageData)}
+          className={
+            `gallery__grid__item
+            ${ 
+              index % 5 === 0 && index < portfolioData.length - 3 ? ' gallery__grid__item--double-row' :
+              (index ) % 3 === 0 && index < portfolioData.length - 7 ? ' gallery__grid__item--double-column' :
+              ' gallery__grid__item--single' }          
+            `}>
+          <img src={imageData.imgFileThumb} alt={imageData.imgAlt} />
+          <div className='gallery__grid__item__desc'>
+            <h4>{ firstLetterUpper(imageData.title) }</h4>
+            <p>{ imageData.released.getFullYear() }</p>
+            <ul className='icon-custom__list'>
+              { imageData.stack.map((stackItem) => (
+                <li key={stackItem.toString} className="icon-custom">
+                  <i className={`icon-custom__container ${ stackItem.iconClass }`}></i>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      ) : null }     
-      <div className='gallery__grid'>
-        { portfolioData.map(({ id, imgFileThumb, imgAlt, title, released, stack }, index) => (
-          <div key={id} 
-            className={
-              `gallery__grid__item
-              ${ 
-                index % 5 === 0 && index < portfolioData.length - 3 ? ' gallery__grid__item--double-row' :
-                (index ) % 3 === 0 && index < portfolioData.length - 7 ? ' gallery__grid__item--double-column' :
-                ' gallery__grid__item--single' }          
-              `}>
-            <img src={imgFileThumb} alt={imgAlt} />
-            <div className='gallery__grid__item__desc'>
-              <h4>{ firstLetterUpper(title) }</h4>
-              <p>{ released.getFullYear() }</p>
-              <ul className='icon-custom__list'>
-                { stack.map((stackItem) => (
-                  <li key={stackItem.toString} className="icon-custom">
-                    <i className={`icon-custom__container ${ stackItem.iconClass }`}></i>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
+      ))}
     </div>
   );
 }
--- src/pages/PortfolioCGI/index.tsx
+++ src/pages/PortfolioCGI/index.tsx
@@ -7,60 +7,76 @@ import { PortfolioImagesData, SliderImagesData } from '~/__mock__/data/3d.projec
 
 import './style.scss'
 import Gallery from '~/components/Gallery';
+import { PortfolioData } from '~/interfaces/Data.intf';
+import ImageViewer from '~/components/ImageViewer';
 
 const PortfolioCGI: React.FunctionComponent<PageProps> = ({title = 'titre de la page'}) => {
   const [scrollY, setScrollY] = useState<number>()
+  const [imgViewerData, setImageViewerData] = useState<PortfolioData>()
+  const [displayImageViewer, setDisplayImageViewer] = useState<boolean>(false)
 
   useEffect(() => {
     document.title = title
   })
 
+  const handleImageViewerOnClose = () => {
+    setDisplayImageViewer(false)
+  }
+
+  const handleGalleryImageOnClick = (imageData: PortfolioData) => {
+    setDisplayImageViewer(true)
+    setImageViewerData(imageData)
+  }
+
   return (
-    <SmoothScroll onChanged={(value) => setScrollY(value)}>
-      <Fragment>
-        <Carousel slides={SliderImagesData} parralaxScrollY={scrollY} /> 
-        <div className='page portfolio-cgi' data-testid='page-portfolio-cgi'> 
-          <section className='section portfolio-cgi__specialities'>          
-            <div className='section__content'>            
-              <h2 className='display1'>Spécialités</h2>
-              <ul>
-                <li>
-                  <img height={90} src={require('../../assets/icons/zbrush.png')} alt="Vue js framework javascript"/>
-                  <p>Zbrush</p>
-                </li>
-                <li>
-                  <img height={90} src={require('../../assets/icons/3dsmax.png')} alt="Vue js framework javascript"/>
-                  <p>3DSmax</p>
-                </li>
-                <li>
-                  <img height={90} src={require('../../assets/icons/vray.png')} alt="Vue js framework javascript"/>
-                  <p>Vray</p>
-                </li>
-                <li>
-                  <img height={90} src={require('../../assets/icons/substance-painter.png')} alt="Vue js framework javascript"/>
-                  <p>Substance</p>
-                </li>
-                <li>
-                  <img height={90} src={require('../../assets/icons/photoshop.png')} alt="Vue js framework javascript"/>
-                  <p>Photoshop</p>
-                </li>
-                <li>
-                  <img height={90} src={require('../../assets/icons/illustrator.png')} alt="Vue js framework javascript"/>
-                  <p>Illustrator</p>
-                </li>         
-              </ul>
-            </div>       
-          </section>
-          <section className='section portfolio-cgi__projects'>
-            <div className='section__content--fullwidth'>
-              <h2 className='display1'>Réalisations</h2>
-              <Gallery portfolioData={ PortfolioImagesData } />
-            </div>          
-          </section>
-          <Footer />      
-        </div>
-      </Fragment>
-    </SmoothScroll>
+    <Fragment >
+      <SmoothScroll onChanged={(value) => setScrollY(value)}>
+        <Fragment>
+          <Carousel slides={SliderImagesData} parralaxScrollY={scrollY} /> 
+          <div className='page portfolio-cgi' data-testid='page-portfolio-cgi'> 
+            <section className='section portfolio-cgi__specialities'>          
+              <div className='section__content'>            
+                <h2 className='display1'>Spécialités</h2>
+                <ul>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/zbrush.png')} alt="Vue js framework javascript"/>
+                    <p>Zbrush</p>
+                  </li>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/3dsmax.png')} alt="Vue js framework javascript"/>
+                    <p>3DSmax</p>
+                  </li>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/vray.png')} alt="Vue js framework javascript"/>
+                    <p>Vray</p>
+                  </li>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/substance-painter.png')} alt="Vue js framework javascript"/>
+                    <p>Substance</p>
+                  </li>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/photoshop.png')} alt="Vue js framework javascript"/>
+                    <p>Photoshop</p>
+                  </li>
+                  <li>
+                    <img height={90} src={require('../../assets/icons/illustrator.png')} alt="Vue js framework javascript"/>
+                    <p>Illustrator</p>
+                  </li>         
+                </ul>
+              </div>       
+            </section>
+            <section className='section portfolio-cgi__projects'>
+              <div className='section__content--fullwidth'>
+                <h2 className='display1'>Réalisations</h2>
+                <Gallery portfolioData={ PortfolioImagesData } onClick={ (imageData) => handleGalleryImageOnClick(imageData) } />
+              </div>          
+            </section>
+            <Footer />      
+          </div>        
+          </Fragment>      
+        </SmoothScroll>
+      <ImageViewer imageData={imgViewerData} displayOn={displayImageViewer} onClose={handleImageViewerOnClose} />
+    </Fragment>
   );
 }
 
