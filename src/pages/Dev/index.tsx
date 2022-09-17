2f1babc937fae0b317aaea44e70e42444b9133e0
Stéphane <stephane1920@gmail.com>
2022-09-17 16:12:37 +0200

    optimize images

--- src/pages/Dev/index.tsx
+++ src/pages/Dev/index.tsx
@@ -104,9 +104,11 @@ const Dev: FunctionComponent<PageProps> = ({title = 'titre de la page'}) => {
             <h2 className={`display1 reveal${ appearSectionProjects && contentLoaded ? ' reveal--0' : '' }`}>Réalisations</h2>
             <ul className="portfolio-dev__projects__list">
               {ProjectsDevData.map( (project, index) => (
-                <Link className="portfolio-dev__projects__item" key={`${project.id}`} to={`${project.hashName}`} onClick={() => { window.scrollTo(0,0) }}>
-                  <li><ProjectCard projectData={project} className={appearSectionProjects ? `project-card-container__reveal--${index}` : `project-card-container__hide--${index}`} /></li>
-                </Link>
+                <li className="portfolio-dev__projects__item" key={`${project.id}`}>
+                  <Link   to={`${project.hashName}`} onClick={() => { window.scrollTo(0,0) }}>
+                    <ProjectCard projectData={project} className={appearSectionProjects ? `project-card-container__reveal--${index}` : `project-card-container__hide--${index}`} />
+                  </Link>
+                </li>
               ))}
             </ul>
           </section>
