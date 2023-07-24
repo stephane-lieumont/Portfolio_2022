261770bde22d31e5039670734edb96fff329f3ed
Stephane Lieumont <stephane1920@gmail.com>
2023-07-24 07:51:56 +0200

    feat: update CV 2023

--- src/utils/downloadCV.ts
+++ src/utils/downloadCV.ts
@@ -1,6 +1,6 @@
 export const downloadCV = (callback?: Function) => {
   const element = document.createElement("a");
-  element.href = require('~/assets/pdf/CV_LIEUMONT stephane_2022_FrontEnd.pdf');
+  element.href = require('~/assets/pdf/CV_LIEUMONT stephane_2023_FrontEnd.pdf');
   element.download = `CV_LIEUMONT-stephane_${ new Date().getFullYear() }_FrontEnd.pdf`;
   document.body.appendChild(element);
   element.click();
